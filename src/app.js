3abd1d89d33f576b3838eba0710545735185c90c
Garrett <guitarguy13@ymail.com>
2019-07-23 08:26:12 -0500

    Made it to eslintConfit in FEM and added gitIgnore

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "dash",
+      animal: "dog",
+      breed: "poodle"
+    }),
+    React.createElement(Pet, {
+      name: "frankie",
+      animal: "Also dog",
+      breed: "poodle"
+    }),
+    React.createElement(Pet, {
+      name: "jax",
+      animal: "Cat",
+      breed: "orange"
+    })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
